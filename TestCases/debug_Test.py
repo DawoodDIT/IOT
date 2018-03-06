6b5ec60bfa939fa6e6458dd6ee46214c038bb688
vipin <vipin.kumar@slr-consultants.com>
2018-03-06 10:54:06 +0000

    Debug file added:Vipin

---
+++ TestCases/debug_Test.py
@@ -0,0 +1,38 @@
+import Class.Automation
+import Module.Report
+
+
+obj = Class.Automation.Automation()
+Module.Report.initiateJVM()
+Module.Report.createTestReport("Report","vipintest",False)
+obj.openURL()
+obj.login()
+obj.clickOnLink("PPVGE as Operator Administrator")
+obj.clickOnLink("AutoCust_Event as Customer Administrator")
+obj.verifyTextOnScreen("M2M")
+obj.clickOnLink("AutoCust_Event as Customer Administrator")
+obj.clickOnLink("PPVGE as Operator Administrator")
+obj.verifyTextOnScreen("M2M")
+obj.clickOnLink("PPVGE as Operator Administrator")
+obj.clickOnLink("AutoCust_Event as Customer Administrator")
+obj.verifyTextOnScreen("M2M")
+obj.clickOnLink("AutoCust_Event as Customer Administrator")
+obj.clickOnLink("PPVGE as Operator Administrator")
+# obj.clickOnMenu("Devices")d
+# obj.clickOnSubmeu("All devices")
+# obj.verifyTextOnScreen("Results")
+# obj.clickOnButton("Search")
+# obj.clickOnLink("111110001000002")
+# obj.verifyTextOnScreen("Overview")
+# obj.clickOnTab("Operations")
+# obj.clickOnLink("Configure")
+# obj.verifyTextOnScreen("Change state")
+# obj.clickOnButton("Change...")
+#obj.clickOnButton("Change...",2)
+# obj.verifyTextOnScreen("Basic information")
+# obj.enterText("Name","AutoTest1")
+# obj.enterText("Description","Autoamtion test")
+# obj.clickOnInput("Parent organisation","PPTOMTOM")
+# obj.selectRadioButton("Yes")
+# obj.selectRadioButton("Yes",2)
+
